75aa0b0515ff94b2f4727df206158fc16e16c54b
fablopvaz <fabiolopezvazquez@gmail.com>
2017-12-29 14:53:18 +0100

    server.c fixed

--- server/src/server.c
+++ server/src/server.c
@@ -14,9 +14,6 @@
 /*** SERVER MAIN PROCESS *************/
 int main(void) {
 	pid_t pid = -1; // child/parent process id
-	pthread_attr_t attr;
-	pthread_t thread;
-	Conn * info = malloc(MAX_USER_SIZE + sizeof(int));
 
 	// Create the needed resources
 	init();
@@ -29,13 +26,6 @@ int main(void) {
 			break;
 		case 0: // child process
 			login()
-			/*Set atributes to the thread*/
-			pthread_attr_init(&attr);
-			pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
-
-			/*Creat thread that execute th_controller function*/
-			pthread_creat(thread,&attr,th_controller,info);
-
 			break;
 		default: // parent process
 			menu();
