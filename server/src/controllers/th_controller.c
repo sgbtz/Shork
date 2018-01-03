eba2022d81343f1ad57831bdd98bfc4ac9220113
fablopvaz <fabiolopezvazquez@gmail.com>
2018-01-03 14:18:42 +0100

    th_controller almost ready

--- server/src/controllers/th_controller.c
+++ server/src/controllers/th_controller.c
@@ -2,112 +2,122 @@
 #include <stdio.h>
 #include <sys/msg.h>
 #include <unistd.h> 
-#include <sys/stat.h>
-#include <sys/mman.h>
 #include <string.h>
+#include <semaphore.h>
+
 
 /***APPLICATION FILES**/
 //#include "./log_controller"
+#include "../server.h"
 #include "./file_controller.c"
+#include "./user_controller.c"
 
 
 /***DEFINES***/
+#define MAX_USER_SIZE MAX_USER_SIZE MAX_UNAME + MAX_PASS + MAX_FOLD_URL
+#define MAX_UNAME	16
+#define MAX_PASS	16
+#define MAX_FOLD_URL	64
+#define MAX_OPT MAX_USER_SIZE+MAX_FOLD_URL + sizeof(long)+2*sizeof(unsigned)
+#define MAX_RES sizeof(long)+sizeof(unsigned)
 #define MAX_SEND_SIZE 20
-#define MAX_FILENAME  50
-#define MAP_TIPO 1
+#define MAX_FILENAME  20
 #define ACTION 2
 #define CD 3     /*change directory*/
-#define ISPRIVATE "private"
-#define ISPUBLIC "public"
-#define MOVE "move"
-#define DEL "delete"
-#define DEST_PATH "../res/shared/"
+#define ISPRIVATE 2
+#define ISPUBLIC 1
+#define MOVE 1
+#define DEL 2
+#define RC 1
+#define UPLOAD 1
+#define REQ 1
+#define RES 2
+#define DEST_PRIV_PATH "../res/users/"
+#define DEST_PUBLIC_PATH "../res/users/share/"
 
-void map_type(char * filename,char * perm);
+/***TYPEDEFS******/
+typedef struct{ 
+	long mtype;
+	unsigned scope;
+	unsigned cmd;
+	unsigned ud;
+	User user;
+	char org[MAX_FILENAME];
+	char dest[MAX_FILENAME];
+}Opt;
 
-struct mymsgbuf{ 
-   long mtype;
-   char action[MAX_SEND_SIZE];  /*Especificate what action want to do if type message is ACTION*/
-   char content[MAX_SEND_SIZE]; /*value depends on what function you want to do*/
-   char filename[MAX_SEND_SIZE];
-};
+typedef struct {
+	long mtype;
+	unsigned error;
+}Res;
+
 
 void th_controller(){
 
 key_t key;
 int queue_id;
-long tipo;
-struct mymsgbuf qbuffer;/*Queue buffer*/
+Opt qbuffer;/*Queue buffer*/
+char * org,dest;
+File * file;
+key_t clavem;
+sem_t *mutex=NULL;
+int shmid;
+
 
 key=ftok("../res/share/.",'Q'); /*Creating new queue*/
 
 if((queue_id=msgget(key,IPC_CREAT|0666))==-1) /*get the queue*/
 	printf("Error al iniciar la cola\n");
 else{
-	if(msgrcv(queue_id,&qbuffer,MAX_SEND_SIZE,0,0)!=-1){
-		tipo=qbuffer.mtype;
+	if(msgrcv(queue_id,&qbuffer,MAX_SEND_SIZE,REQ,0)!=-1){
 		/*depending on the type of the msg*/
-		switch(tipo){ 
-			/*MAP THE FILE IN THE PROCESS*/
-			case MAP_TYPE: 
-				map_type(qbuffer.filename,qbuffer.content); /*"private" or "public" as parameter*/ 
-			break;
-			
-			case ACTION:
+		if(qbuffer.scope==ISPRIVATE){
+			switch(qbuffer.cmd)
 				/*MOVE FILE*/
-				if(strcmp(qbuffer.action,MOVE)==0) /*add file or move file case*/
-					move(qbuffer.content,DEST_PATH);
-				/*DELETE FILE*/
-				if(strcmp(qbuffer.action,DEL)==0)/*delete file case*/
-					dlt(DEST_PATH,qbuffer.content);
-			break;
-			
-			//case CD:/*change directory*/
+				case MOVE:
+					org=qbuffer.org;
+					dest=strcat(qbuffer.user.user_folder,qbuffer.dest);
+					move(org,dest);
+			 	break;
 
-			//break
-			default:
-				printf("Tail type incorrect\n");
-			break;
+			 	/*DELETE FILE*/
+			 	case DELETE:
+					dlt(qbuffer.user.user_folder,org);
+			 	break;
+		}
+			
+		if(qbuffer.scope==ISPUBLIC)
+			switch(qbuffer.cmd)
+				clavem=ftok("../res/share/.",'A');
+				co_mm(clavem,TAM_MEMORY);
+				mutex = co_sem("mutex", RC);
+				/*map the memory*/
+				if(file =shmat(shmid,NULL,0) == (File *)-1)
+					printf("Error al mapear la memoria compartida\n");
+				else{
+					sem_wait(mutex);
+					used(/*To-Do*/)
+				}
+
+
+				/*MOVE FILE*/
+				case MOVE:
+					if(qbuffer.ud == UPLOAD)
+						move(qbuffer.org,qbuffer.dest);
+					else{
+						
+					}
+				break;
+				/*DELETE FILE*/
+				case DELETE:
+				break;
+		
 		}
 	}
 }
 
-}
 
 
-void map_type(char * filename,char * perm){
 
-struct stat bstat;
-char * mapped_file;
-int fd; //File descriptor
 
-if((fd=open(filename,O_RDONLY))<0) //First open the file
-	printf("No pudo abrirse el fichero\n");
-	
-else if(fstat(fd,&bstat)){ //get file status
-	printf("Error en el fstat del fichero\n");
-}
-else if(ftruncate(fd,bstat.st_size)<0) //get the size of the file
-	printf("Error en el ftruncate en el fichero\n");
-
-/*Then, map the file in to the process*/
-if(strcmp(perm,ISPRIVATE)==0){ //privated mapping
-	if((mapped_file=(char * )mmap((caddr_t)0,bstat.st_size,PROT_WRITE,MAP_PRIVATE,fd,0))==MAP_FAILED){
-		printf("Error en la proyeccion de fichero\n");
-		printf("File has been mapped privated succesfuly\n");
-	}
-	//Now what??
-	
-//otherwise mapping is public
-else{   
-	if((mapped_file=(char * )mmap((caddr_t)0,bstat.st_size,PROT_WRITE,MAP_SHARED,fd,0))==MAP_FAILED){
-		printf("Error en la proyeccion de fichero\n");
-		printf("File has been mapped shared succesfuly\n");
-	}
-	//Now What???
-	
-}
-
-}
-}
 
